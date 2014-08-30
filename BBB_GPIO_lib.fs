e3670772208fea577ac8ec8a341fee197211fdad
harrypower <philipkingsmith@gmail.com>
2014-08-30 07:21:33 +0000

    still playing around to get the stuff straight and remove fluff

--- BBB_GPIO_lib.fs
+++ BBB_GPIO_lib.fs
@@ -37,12 +37,10 @@ c-library myBBBGPIO
 \c #define GPIO_SIZE  0x00000FFF
 
 \c // OE: 0 is output, 1 is input
-\c // #define GPIO_OE (0x134 / 4)
-\c #define GPIO_OE 0x4d
-\c // #define GPIO_DATAIN (0x138 / 4)
-\c #define GPIO_DATAIN 0x4e
-\c // #define GPIO_DATAOUT (0x13c / 4)
-\c #define GPIO_DATAOUT 0x4f
+\c #define GPIO_OE (0x134 / 4)
+\c #define GPIO_DATAIN (0x138 / 4)
+\c #define GPIO_DATAOUT (0x13c / 4)
+
 
 \c #define GPIO1_28 (1<<28)
 \c #define GPIO0_7  (1<<7)
@@ -51,7 +49,7 @@ c-library myBBBGPIO
 \c char *gpio_mem, *gpio_map;
 \c volatile unsigned *gpio;
 
-\c static int io_setup(void){
+\c static void io_setup(void){
 \c // Enable all GPIO banks
 \c // Without this, access to deactivated banks (i.e. those with no clock source set up) will (logically) fail with SIGBUS
 \c // Idea taken from https://groups.google.com/forum/#!msg/beagleboard/OYFp4EXawiI/Mq6s3sg14HoJ
@@ -66,7 +64,7 @@ c-library myBBBGPIO
 
 \c /* mmap GPIO */
 \c gpio_map = (char *)mmap( 0, GPIO_SIZE, PROT_READ|PROT_WRITE,
-\c     MAP_SHARED, mem_fd, GPIO0_BASE );
+\c     MAP_SHARED, mem_fd, GPIO1_BASE );
 
 \c if (gpio_map == MAP_FAILED) {
 \c     printf("mmap error %d\n", (int)gpio_map);
@@ -79,48 +77,49 @@ c-library myBBBGPIO
 \c unsigned int creg = *(gpio + GPIO_OE);
 
 \c // set output
-\ \c creg = creg & (~GPIO1_28);
-\c creg = creg & (~GPIO0_7);
+\c creg = creg & (~GPIO1_28);
+\ \c creg = creg & (~GPIO0_7);
 \c *(gpio + GPIO_OE) = creg;
 
 \c int i;
 \c int test;
     
-\c for( i = 0; i < 100000 ; i++ ){
-\ \c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) | GPIO1_28;
-\c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) | GPIO0_7;
-\c     usleep(1);
-\ \c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) & (~GPIO1_28);
-\c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) & (~GPIO0_7);
-\c     usleep(1); }
+\c for( i = 0; i < 10000 ; i++ ){
+\c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) | GPIO1_28;
+\ \c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) | GPIO0_7;
+\ \c     usleep(1);
+\c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) & (~GPIO1_28);
+\ \c     *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) & (~GPIO0_7);
+\ \c     usleep(1);
+\c } }
 
-\c return ( 0 ) ;
-\c }
+\c void teston(void)  { *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) | 0x10000000; }
+
+\c void testoff(void) { *(gpio + GPIO_DATAOUT) = *(gpio + GPIO_DATAOUT) & ~0x10000000; }
+
+\c int clock(void) { return ( (int) *(gpio + (0x130 / 4))); }
+
 \c 
-\c static int gpiosetup(void) { return 0; }
+\c int gpiosetup(int gb) { return 0; }
 \c int gpiocleanup(void) { return 0; }
-\ \c int gpiopullup(int gb, int gp) { return 0; }
-\ \c int gpiopulldown(int gb, int gp) { return 0; }
-\ \c int gpiopulloff(int gb, int gp) { return 0; }
 \c int gpioinput(int gb, int gp) { return 0; }
 \c int gpiooutput(int gb, int gp) { return 0; }
-\c static int gpioread(int *error, int gb, int gp) { return 0; }
+\c int gpioread(int *error, int gb, int gp) { return 0; }
 \c int gpioset(int gb, int gp) { return 0; }
 \c int gpioclear(int gb, int gp) { return 0; }
 
-c-function GPIO-setup   io_setup          -- n 
+c-function clockdata    clock             -- n
+c-function teston       teston            -- void 
+c-function testoff      testoff           -- void
+c-function GPIO-setup   io_setup          -- void 
 
-c-function bgiosetup    gpiosetup         -- n
+c-function bgiosetup    gpiosetup       n -- n
 c-function bgiocleanup  gpiocleanup       -- n
-\ c-function bgiopullup   gpiopullup    n n -- n
-\ c-function bgiopulldown gpiopulldown  n n -- n
-\ c-function bgiopulloff  gpiopulloff   n n -- n
 c-function bgioinput    gpioinput     n n -- n
 c-function bgiooutput   gpiooutput    n n -- n
 c-function bgioread     gpioread    a n n -- n
 c-function bgioset      gpioset       n n -- n
 c-function bgioclear    gpioclear     n n -- n
-
 
 end-c-library
 
